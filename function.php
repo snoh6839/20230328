b75d34d22b76e3088d30cdb336b6723e28faa662
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-03-28 14:32:09 +0900

    재귀함수 과일 재고 관리

--- function.php
+++ function.php
@@ -133,4 +133,52 @@ function func_args_divid()
 }
 echo func_args_divid(1, 3, 4, 2) . "\n";
 
+function repeatMultipl($i) {
+            if ($i > 0) {
+                return $i * repeatMultipl($i - 1);
+            }
+            return 1;
+        }
+echo repeatMultipl(5)."\n";
+
+function repeatSum($i)
+{
+    if ($i > 0) {
+        return $i + repeatSum($i - 1);
+    }
+    return 0;
+}
+echo repeatSum(5) . "\n";
+
+
+$fruits = [
+    '사과' => 10,
+    '배' => 5,
+    '수박' => 2,
+    '포도' => 0,
+];
+
+function checkFruit(&$fruits, $fruitName)
+{
+    if (array_key_exists($fruitName, $fruits)) {
+        if ($fruits[$fruitName] > 0) {
+            $fruits[$fruitName] --;
+            echo "{$fruitName} 한 개를 꺼냈습니다. 남은 수량: {$fruits[$fruitName]}\n";
+        } else {
+            echo "{$fruitName}은(는) 더 이상 남아있지 않습니다.\n";
+        }
+    } else {
+        echo "{$fruitName}은(는) 존재하지 않는 과일입니다.\n";
+    }
+}
+
+checkFruit($fruits, '사과');
+checkFruit($fruits, '배');
+checkFruit($fruits, '수박');
+checkFruit($fruits, '포도');
+checkFruit($fruits, '망고');
+
+checkFruit($fruits, '사과');
+
+
 ?>
