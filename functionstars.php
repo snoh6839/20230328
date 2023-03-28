399653cc7f76ea0ac69bfa20ccdebbbee33e4c26
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-03-28 18:10:20 +0900

    kkk

---
+++ functionstars.php
@@ -0,0 +1,54 @@
+<?php
+
+function stars($in_num)
+{
+    for ($i=1; $i <= $in_num; $i++) { 
+        for ($j=0; $j < $i; $j++) { 
+            echo "*";
+        }
+        echo "\n";
+    }
+}
+
+// stars(5);
+
+function stars2($in_num)
+{
+    for ($i = 1; $i <= $in_num; $i++) {
+            echo "*";
+    }
+}
+
+// stars2(4);
+
+// function stars3(...$rows)
+// {
+//     foreach ($rows as $rows) {
+//         for ($i = 1; $i <= $rows; $i++) {
+//             echo "*";
+//         }
+//         echo "\n";
+//     }
+// }
+
+// stars3(1,2,3,4,5);
+
+
+function printStars($rows)
+{
+    for ($i = 0; $i <= $rows; $i++) {
+        echo '*';
+    }
+    echo "\n";
+}
+
+$rows = 5;
+
+for ($k=0; $k < $rows; $k++) {
+    printStars($k);
+}
+
+for ($j=0; $j < $rows;) {
+    printStars($j);
+}
+?>
