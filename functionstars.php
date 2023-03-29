ef6cbcf2ee4e00f8f8bbe8a62be77bf6b651434c
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-03-29 09:46:12 +0900

    정리

--- functionstars.php
+++ functionstars.php
@@ -1,38 +1,4 @@
 <?php
-
-// function stars($in_num)
-// {
-//     for ($i=1; $i <= $in_num; $i++) { 
-//         for ($j=0; $j < $i; $j++) { 
-//             echo "*";
-//         }
-//         echo "\n";
-//     }
-// }
-
-// stars(5);
-
-// function stars2($in_num)
-// {
-//     for ($i = 1; $i <= $in_num; $i++) {
-//             echo "*";
-//     }
-// }
-
-// stars2(4);
-
-// function stars3(...$rows)
-// {
-//     foreach ($rows as $rows) {
-//         for ($i = 1; $i <= $rows; $i++) {
-//             echo "*";
-//         }
-//         echo "\n";
-//     }
-// }
-
-// stars3(1,2,3,4,5);
-
 //1
 
 
